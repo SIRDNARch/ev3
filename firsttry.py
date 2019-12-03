bbd339d13518e581d9a181ea403894c104250242
th259 <58256940+th259@users.noreply.github.com>
2019-12-03 16:21:48 +0100

    Update firsttry.py

--- firsttry.py
+++ firsttry.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
-from ev3dev2.sensor import INPUT_1
-from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3
+from ev3dev2.sensor.lego import TouchSensor, ColorSensor
 from ev3dev2.led import Leds
 import time
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
 
-ts = TouchSensor()
-leds = Leds()
 
-def main():
+colour_s1 = ColorSensor() # sensor 1 for light intensity
+colour_s1.MODE_COL_REFLECT = "COL-REFLECT"
+
+tank_drive = MoveTank(OUTPUT_A,OUTPUT_B) # left motor on PORT A right motor on PORT B
+
+def main():   
+    a = 0.6
+    
+    d = a * (5 - colour_s1.reflected_light_intensity())
+ 
     while True:
-        if ts.is_pressed:
-            leds.set_color("LEFT", "GREEN")
-            leds.set_color("RIGHT", "GREEN")
-            tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
-            tank_drive.on(SpeedPercent(100), SpeedPercent(100))
-        else:
-            leds.set_color("LEFT", "RED")
-            leds.set_color("RIGHT", "RED")
-            tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
-            tank_drive.on(SpeedPercent(0), SpeedPercent(0))
-
-
+        if (5  - colour_s1.reflected_light_intensity()) > d:
+      
+            tank_drive.on_for_rotations(SpeedPercent(100),SpeedPercent(0), 2) # maybe change LEFT AND RIGHT
+            
+        elif (colour_s1.reflected_light_intensity()  - 5) > d:
+         
+            tank_drive.on_for_rotations(SpeedPercent(0),SpeedPercent(100), 2) # maybe change LEFT AND RIGHT
+            
+        elif (5  - colour_s1.reflected_light_intensity()) <= d or (5  - colour_s1.reflected_light_intensity() * -1 <= d):
+        
+            tank_drive.on_for_rotations(SpeedPercent(100),SpeedPercent(100), 2)
+        
 if __name__ == '__main__':
     main()
