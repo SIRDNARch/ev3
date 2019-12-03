67d38817e5df79ae3f6beb0266d9b106d3df09d7
th259 <58256940+th259@users.noreply.github.com>
2019-12-03 16:21:52 +0100

    Update followline.py

--- followline.py
+++ followline.py
@@ -7,16 +7,16 @@ from time import sleep
 
 # TODO: Add code here
 
-    colour_s1 = ColorSensor(INPUT_1) # sensor 1 for light intensity
-    colour_s2 = ColorSensor(INPUT_2) # sensor 2 for light intensity
+colour_s1 = ColorSensor(INPUT_1) # sensor 1 for light intensity
+colour_s2 = ColorSensor(INPUT_2) # sensor 2 for light intensity
         
-    lm = LargeMotor(OUTPUT_B);  # left motor on PORT B
-    rm = LargeMotor(OUTPUT_C);  # right motor on PORT C
+lm = LargeMotor(OUTPUT_B)  # left motor on PORT B
+rm = LargeMotor(OUTPUT_C)  # right motor on PORT C
     
-    a = 0.6;
+a = 0.6
     
-    d = a * (colour_s2.reflected_light_intensity()  - colour_s1.reflected_light_intensity())
-    
+d = a * (colour_s2.reflected_light_intensity() - colour_s1.reflected_light_intensity())
+def main():    
     while True:
 
         if (colour_s2.reflected_light_intensity()  - colour_s1.reflected_light_intensity()) > d:
@@ -25,12 +25,15 @@ from time import sleep
             rm.on_for_rotations(SpeedPercent(0), 2)
             
         elif (colour_s1.reflected_light_intensity()  - colour_s2.reflected_light_intensity()) > d:
-            
+           
             lm.on_for_rotations(SpeedPercent(0), 2) # maybe change LEFT AND RIGHT
             rm.on_for_rotations(SpeedPercent(100), 2)
             
         elif ((colour_s2.reflected_light_intensity()  - colour_s1.reflected_light_intensity()) <= d
-                or (colour_s2.reflected_light_intensity()  - colour_s1.reflected_light_intensity()) * -1 <= d):
+                 or (colour_s2.reflected_light_intensity()  - colour_s1.reflected_light_intensity()) * -1 <= d):
         
-            lm.on_for_rotations(SpeedPercent(100), 2)
-            rm.on_for_rotations(SpeedPercent(100), 2)
+                lm.on_for_rotations(SpeedPercent(100), 2)
+                rm.on_for_rotations(SpeedPercent(100), 2)
+        
+if __name__ == '__main__':
+    main()
