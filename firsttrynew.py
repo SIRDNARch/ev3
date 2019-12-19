04b6c3c4f07bb20942d718f7564e24b3ad6faeb9
SIRDNARch <58331483+SIRDNARch@users.noreply.github.com>
2019-12-19 15:29:42 +0100

    Update firsttrynew.py
    firsttrynew updated for 3 sensor use

--- firsttrynew.py
+++ firsttrynew.py
@@ -8,87 +8,122 @@ from ev3dev2.led import Leds
 import time
 
 
-light_s1 = LightSensor(INPUT_1) # sensor 1 for light intensity
-light_s1.MODE_REFLECT = "REFLECT"
-light_s2 = LightSensor(INPUT_2) # sensor 1 for light intensity
-light_s2.MODE_REFLECT = "REFLECT"
-light_s3 = ColorSensor(INPUT_3)
-light_s3.mode = "COL-REFLECT"
-usonic_s1 = UltrasonicSensor(INPUT_4)
+light_left = LightSensor(INPUT_1) #left for light intensity
+light_left.MODE_REFLECT = "REFLECT"
 
+light_right = LightSensor(INPUT_2) #right for light intensity
+light_right.MODE_REFLECT = "REFLECT"
+
+light_mid = ColorSensor(INPUT_3) #mid for light intensity
+light_mid.mode = "COL-REFLECT"
+
+usonic_sensor = UltrasonicSensor(INPUT_4)#Ultrasonic Sensor for object detection
+
+#left motor on PORT A right motor on PORT B
 left_m = LargeMotor(OUTPUT_A)
-right_m = LargeMotor(OUTPUT_B) # left motor on PORT A right motor on PORT B
-tank_drive = MoveTank(OUTPUT_A,OUTPUT_B) # left motor on PORT A right motor on PORT B'''
-state = 0
+right_m = LargeMotor(OUTPUT_B)
+tank_drive = MoveTank(OUTPUT_A,OUTPUT_B) 
 
 
 def main():
     a = 0.4 #constant for the accuracy
-    leftspeed =  SpeedPercent(40) # Speed in percent when driving straight
-    rightspeed = SpeedPercent(40)
-    #Button has to be pressed to start the initialization of d
+    max_speed = SpeedPercent(100)
+    left_speed = SpeedPercent(40)
+    right_speed = SpeedPercent(40)
+    #beep when ready
     Sound().beep()
+    #button has to be pressed to start the initialization of d
     while not Button().any():
         continue
-    d = a * (light_s2.reflected_light_intensity - light_s1.reflected_light_intensity)
-    black = light_s1.reflected_light_intensity * 1.1
+    white = (light_right.reflected_light_intensity + light_left.reflected_light_intensity)/2
+    black = light_mid.reflected_light_intensity
+    d = white - black * a # difference between white and black
+    two_white = (2*white + black)/3
+    two_black = (white + 2*black)/3
 
-    #time.sleep(2)
-    #Button has to be pressed to start the while loop with the follow line procedure
+    #button has to be pressed to start the while loop with the follow line procedure
     while not Button().any():
         continue
-
+    #beed when done with initialization
     Sound().beep()
-    #time.sleep(2)
 
     while True:
-
+        #follow line till sensor detects object
         while not object_detection(20):
-            s1 = light_s1.reflected_light_intensity
-            s2 = light_s2.reflected_light_intensity
-            follow_line(s1, s2, leftspeed, rightspeed, d, 0)
+            follow_line(light_left.reflected_light_intensity, light_right.reflected_light_intensity, light_mid.reflected_light_intensity, max_speed, left_speed, right_speed, d, two_black, two_white)
+
+        # turn around
         left_m.polarity = "inversed"
-        tank_drive.on_for_rotations(leftspeed, rightspeed, 1.6)
+        tank_drive.on_for_rotations(left_speed, right_speed, 1.6)
         left_m.polarity="normal"
         time.sleep(0.5)
 
+        #follow line till sensor detects object
         while not object_detection(20):
-            s1 = light_s1.reflected_light_intensity
-            s2 = light_s2.reflected_light_intensity
-            follow_line(s1, s2, leftspeed, rightspeed, d,0)
+            follow_line(light_left.reflected_light_intensity, light_right.reflected_light_intensity, light_mid.reflected_light_intensity, max_speed, left_speed, right_speed, d, two_black, two_white)
 
+        #wait till object gets removed
         while object_detection(20):
             tank_drive.off()
-            time.sleep(1)
+            time.sleep(0.5)
 
-        test = True
-        while test:
-            if light_s1.reflected_light_intensity <= black and light_s2.reflected_light_intensity <= black and light_s3.reflected_light_intensity <= black or object_detection(12):
-                Sound().beep()
-                test = False
-            s1 = light_s1.reflected_light_intensity
-            s2 = light_s2.reflected_light_intensity
-            follow_line(s1, s2, leftspeed, rightspeed, d, 0.2)
-
+        #follow till sensor detect all black then stop !
+        while not follow_line(light_left.reflected_light_intensity, light_right.reflected_light_intensity, light_mid.reflected_light_intensity, max_speed, left_speed, right_speed, d, two_black, two_white) == 3:
+            continue
         tank_drive.off()
+        return # end of test line / stop program
 
-        return
 
 def object_detection(x: int) -> bool:
-    if usonic_s1.distance_centimeters <= x:
+    if usonic_sensor.distance_centimeters <= x:
         return True
     else:
         return False
 
-def follow_line(s1: int, s2: int, leftspeed: float, rightspeed: float, d: float, t: float) -> int:
-    if(s2-s1) > d:
+def follow_line(left: int, right: int, mid: int, max_speed: float, left_speed: float, right_speed: float, d: float, two_black: float, two_white: float) -> int:
+    if left-mid > d and right - mid > d: # left AND right white, mid black
+        tank_drive.on(max_speed/2.5, max_speed/2.5)
+        return 0
+    if(right-left) > d: #left black
         left_m.off()
-        time.sleep(t)
+        if(mid-left) > d: #mid white
+            right_m.on(max_speed/2)
+        else:              #mid black
+            right_m.on(max_speed/3)
         return 1
-    if(s1-s2) > d:
+    if(left-right) > d: #right black
         right_m.off()
+        if(mid-right) > d: #mid white
+            left_m.on(max_speed/2)
+        else:              #mid black
+            left_m.on(max_speed/3)
         return 2
-    tank_drive.on(leftspeed, rightspeed)
-    return 3
+    if (left+right+mid)/3 < two_black: #all three black
+        return 3
+        #return 4
+    if (left+right+mid)/3 > two_white: #all three white
+        #try to detect code
+        left_m.polarity = "inversed"
+        right_m.polarity = "inversed"
+        tank_drive.on(max_speed/2, max_speed/2)
+        while light_left.reflected_light_intensity > two_black and light_mid.reflected_light_intensity > two_black and light_right.reflected_light_intensity > two_black:
+            continue
+        time.sleep(0.5)
+        if abs(light_left.reflected_light_intensity - light_mid.reflected_light_intensity) < d or abs(light_right.reflected_light_intensity - light_mid.reflected_light_intensity):
+            return 5
+        #code detected then go right PROBABLY NOT CORRECT NEEDS TO BE TESTED MAYBE DO IT IN main() method
+        left_m.polarity = "normal"
+        right_m.polarity = "normal"
+        tank_drive.on(max_speed/2, max_speed/2)
+        time.sleep(2)
+        right_m.polarity = "inversed"
+        tank_drive.on(0.65*max_speed, 0.65*max_speed)
+        time.sleep(2)
+
+        while (left+right+mid)/3 > two_white:
+            continue
+        return 6
+    return 7 #if something went wrong
+
 if __name__ == '__main__':
     main()
